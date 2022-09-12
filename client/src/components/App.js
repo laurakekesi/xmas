8a4ddf87352930e2c64cf55113faad8bb0cfd3f9
“Laura <laurakekesi@gmail.com>
2022-09-12 16:33:55 -0400

    Day 1

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Homepage from './Homepage';
+
+const App = () => {
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Homepage />}/>
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
